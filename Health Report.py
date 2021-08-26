a9cfcf8ce8bce654f123f7d075b7b0b5327c5fab
inetgeek <42563262+Inetgeek@users.noreply.github.com>
2021-08-27 04:18:34 +0800

    Update Health Report.py

--- Health Report.py
+++ Health Report.py
@@ -19,20 +19,27 @@ def get_code(url):
     driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[3]/div/form/p[2]/input[1]').send_keys(key) #注入密码
     driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[3]/div/form/p[5]/button').click() #点击登录按钮
     time.sleep(15) 
-    try:#捕获异常
+    try:#捕获异常，若已打卡则返回2
         driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div[2]/div/span")
-        return 2
-    except: #没打过卡则进行下一步填表操作
-        Select(driver.find_element_by_xpath('/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[9]/td[2]/div/select')).select_by_value("35.5")#选择下拉列表35.5
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="V1_CTRL142"]').click()#勾选承诺
-        time.sleep(5)
-        driver.find_element_by_xpath('/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[3]/ul/li/a').click()#点击提交按钮
-        time.sleep(5)
-        driver.find_element_by_xpath('/html/body/div[7]/div/div[2]/button[1]').click()#点击“好”，完成提交
         display.stop()
-        driver.quit() #退出操作
-        return 1 #进行到这一步则说明正常提交，未抛出异常，返回1
+        driver.quit()
+        return 2
+    except:#捕获异常，若未打卡则进行下一步填表操作
+        try:#捕获异常，若能正常填写且能正常提交则返回1
+            Select(driver.find_element_by_xpath('/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[9]/td[2]/div/select')).select_by_value("35.5")#选择下拉列表35.5
+            time.sleep(2)
+            driver.find_element_by_xpath('//*[@id="V1_CTRL142"]').click()#勾选承诺
+            time.sleep(5)
+            driver.find_element_by_xpath('/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[3]/ul/li/a').click()#点击提交按钮
+            time.sleep(5)
+            driver.find_element_by_xpath('/html/body/div[7]/div/div[2]/button[1]').click()#点击“好”，完成提交
+            display.stop()
+            driver.quit()
+            return 1
+        except:#捕获异常，若不能正常填写或不能正常提交则返回0
+            display.stop()
+            driver.quit()
+            return 0
 
 status = get_code(url) #获得填表情况的状态
 
@@ -69,20 +76,25 @@ def sender_mail(): #构造并发送邮件
 Now_Date = getNowDate()
 date = '<span style="color:#FC5531">'+Now_Date+'</span>'
 
-if __name__ == "__main__":
+if __name__ == "__main__": #主函数入口
     send_title = "向“理”报平安打卡状态"
-    content_0 = '<p style="color:#507383">亲爱的主人：</p><p style="font-size:34px;color:#3095f1;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">今日已上报，无需进行任何操作！</span></p>'+date
-    content_1 = '<p style="color:#507383">亲爱的主人：</p><p style="font-size:34px;color:#5fa207;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">打卡成功，无需进行任何操作！</span></p>'+date
-    content_2 = '<p style="color:#507383">亲爱的主人：</p><p style="font-size:34px;color:#ca1b0f;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">打卡失败，请及时手动打卡！</span></p>'+date
-    if status == 2:
+    send_head = '<p style="color:#507383">亲爱的主人：</p>'
+    content_0 = send_head+'<p style="font-size:34px;color:#3095f1;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">今日已上报，无需进行任何操作！</span></p>'+date
+    content_1 = send_head+'<p style="font-size:34px;color:#5fa207;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">打卡成功，无需进行任何操作！</span></p>'+date
+    content_2 = send_head+'<p style="font-size:34px;color:#ca1b0f;"><span style="border-bottom: 1px dashed #ccc; z-index: 1; position: static;">打卡失败，请及时手动打卡！</span></p>'+date
+    if status == 2: #已打过卡
         send_content = content_0
         sender_mail()
         print("今日已上报，无需进行任何操作！")
-    elif status == 1:
+    elif status == 1: #打卡成功
         send_content = content_1
         sender_mail()
         print("打卡成功，无需进行任何操作！")
-    else:
+    elif status == 0: #由于没有正常填写或提交导致的打卡失败
+        send_content = content_2
+        sender_mail()
+        print("打卡失败，请及时手动打卡！")
+    else: #未知原因导致的打卡失败
         send_content = content_2
         sender_mail()
         print("打卡失败，请及时手动打卡！")
